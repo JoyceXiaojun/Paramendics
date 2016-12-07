4dc9a7116d3758a1a2897d89e93b8ae61e735cd5
QSSS <zqs901103@gmail.com>
2016-12-06 17:07:12 -0800

    revert

--- paramedic/static/js/report.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by zxc on 16/12/4.
- */
-function send_report() {
-    window.location.href = '/paramedic/home';
-}
-function back() {
-    window.location.href = '/paramedic/home';
-}
